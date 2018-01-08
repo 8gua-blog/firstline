ca651780ae3cf4665ce13a1976a3f0d965613c4d
8gua-men <35199489+8gua-men@users.noreply.github.com>
2018-01-08 12:46:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,28 +2,31 @@
 
 var fs = require('fs');
 
-module.exports = function (path) {
-  return new Promise(function (resolve, reject) {
-    var rs = fs.createReadStream(path, {encoding: 'utf8'});
-    var acc = '';
-    var pos = 0;
-    var index;
-    rs
-      .on('data', function (chunk) {
-        index = chunk.indexOf('\n');
-        acc += chunk;
-        if (index === -1) {
-          pos += chunk.length;
-        } else {
-          pos += index;
-          rs.close();
-        }
-      })
-      .on('close', function () {
-        resolve(acc.slice(0, pos));
-      })
-      .on('error', function (err) {
-        reject(err);
-      })
-  });
+module.exports = function(path) {
+    return new Promise(function(resolve, reject) {
+        var rs = fs.createReadStream(path, {
+            encoding: 'utf8'
+        });
+        var acc = '',
+            r;
+        var pos = 0;
+        var index;
+        rs.on('data', function(chunk) {
+            index = chunk.indexOf('\n');
+            acc += chunk;
+            if (index >= 0) {
+                r = acc.slice(0, pos + index).trim()
+                if (r.length) {
+                    rs.close();
+                }
+            }
+            pos += chunk.length;
+        })
+            .on('close', function() {
+            resolve(r || acc.trim());
+        })
+            .on('error', function(err) {
+            reject(err);
+        })
+    });
 };
